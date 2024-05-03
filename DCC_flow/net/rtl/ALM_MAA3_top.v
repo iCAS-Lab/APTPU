d920762f69d98c351bbc29a36d526a49a47485b3
Mohammed E. Elbtity <elbtity@ieee.org>
2024-05-03 03:09:52 -0400

    License: Adding MIT License Headers to all Verilog files

--- DCC_flow/net/rtl/ALM_MAA3_top.v
+++ DCC_flow/net/rtl/ALM_MAA3_top.v
@@ -1,8 +1,27 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// MIT License
 // 
+// Copyright (c) 2021-2024 iCAS Lab
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Company: iCAS Lab, University of South Carolina
+// Engineer: Mohammed E. Elbtity
 // Create Date: 06/15/2023 04:13:38 AM
 // Design Name: 
 // Module Name: ALM_MAA3_top
@@ -18,6 +37,8 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
 
 module ALM_MAA3_top #(parameter A_BW = 32, B_BW = 32, LOG2_WIDTH = $clog2(A_BW), WIDTH = 2**LOG2_WIDTH, M = 6)
 (
--- DCC_flow/net/rtl/multiplication_unit.v
+++ DCC_flow/net/rtl/multiplication_unit.v
@@ -1,8 +1,27 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
+// MIT License
+// 
+// Copyright (c) 2021-2024 iCAS Lab
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Company: iCAS Lab, University of South Carolina
 // Engineer: Mohammed E. Elbtity
-// 
 // Create Date: 02/26/2021 03:55:26 PM
 // Design Name: 
 // Module Name: multiplication_unit
@@ -19,6 +38,9 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
+
+`timescale 1ns / 1ps
 
 module multiplication_unit #(
     parameter A_BW=8,
